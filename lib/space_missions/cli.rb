a39490ea91a4663434f72a281006663f7ae9c87c
joan <joanrigdon@gmail.com>
2019-03-06 14:22:22 -0500

    adding CLI file

---
+++ lib/space_missions/cli.rb
@@ -0,0 +1,43 @@
+#CLI Controller
+class space_missions::CLI
+
+  def call
+    list_missions
+    menu
+    goodbye
+  end
+
+  def list_missions
+    puts "JPL's current Space Missions:"
+    puts <<-DOC.gsub /^\s*/, '' #(heredoc)
+      1. ASTER - Advanced Spaceborne Thermal Emission and Reflection Radiometer
+      2. ASO - Airborne Snow Observatory
+      3. AVIRIS-NG - Airborne Visible-Infrared Imaging Spectrometer - Next Generation
+    DOC
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      puts "Enter the number of the mission you'd like to learn more about. You can also type 'list' to see the missions again, or type 'exit' "
+      case input
+      when "1'
+        puts""More info on mission 1"
+      when "2"
+        puts "More info on mission 2"
+      when "3"
+        puts "More info on mission 3"
+      when "list"
+        list_missions
+      else
+        puts "Whoops! That's number of a mission. Please try again."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thanks for visiting!"
+  end
+
+end
