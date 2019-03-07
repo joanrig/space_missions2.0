47498bd28daa32bcb2888a4e6579fcfc4114b728
joan <joanrigdon@gmail.com>
2019-03-07 16:53:46 -0500

    improved #self.get_attributes in Scraper class

--- lib/space_missions/mission.rb
+++ lib/space_missions/mission.rb
@@ -1,5 +1,5 @@
 class SpaceMissions::Mission
-  attr_accessor :url, :acronym, :name, :full_name, :description, :attributes, :type, :status, :launch_date, :launch_location, :end_date, :target, :current_location, :altitude, :scientific_instruments
+  attr_accessor :url, :acronym, :name, :description, :attributes, :type, :status, :launch_date, :launch_location, :mission_end_date, :end_date, :target, :current_location, :altitude, :scientific_instruments
 
   @@all = [] #array of all missions
 
@@ -7,18 +7,8 @@ class SpaceMissions::Mission
     @@all << self
   end
 
-
-
-
-
-
-
-
-
   def self.all
     @@all
   end
-
-
 
 end
--- lib/space_missions/scraper.rb
+++ lib/space_missions/scraper.rb
@@ -1,17 +1,14 @@
 class SpaceMissions::Scraper
 
-
-
   def self.get_jpl_mission_links
     @@doc = Nokogiri::HTML(open("https://www.jpl.nasa.gov/missions/"))
     slides = @@doc.css('ul.articles li.slide')
     slides.each do |slide|
       mission = SpaceMissions::Mission.new
       mission.url = slide.css('a').attribute('href').to_s.gsub("http", "https")
-      mission.full_name = @@doc.css('h1.media_feature_title').text.strip
+      mission.name = @@doc.css('h1.media_feature_title').text.strip
       #binding.pry
     end
-
   end
 
   def self.get_attributes
@@ -22,13 +19,15 @@ class SpaceMissions::Scraper
 
       attributes.each do |el|
         key = el.split(":")[0].downcase.gsub(" ","_")
-        value = el.split(":")[1]
-        mission.send("#{key}=",value)
+        value = el.split(":")[1].strip
+        # with .strip /Users/Joan/Dev/space_missions/lib/space_missions/scraper.rb:22:in `block (2 levels) in get_attributes': undefined method `strip' for nil:NilClass (NoMethodError)
+        #without .strip, `_april_03,_2013=' gets sent as key
+        mission.send("#{key}=", value)
       end
+      mission
     end
     binding.pry
   end
-
 
   def self.mission_links
     @@mission_links
