fb8b4eeac28868e54f6921117a73b74b6d13fd3d
joan <joanrigdon@gmail.com>
2019-03-07 01:29:54 -0500

    added css selector for mission links

--- lib/space_missions/mission.rb
+++ lib/space_missions/mission.rb
@@ -1,19 +1,32 @@
+#require 'pry'
+#SpaceMissions::Mission.get_JPL_mission_links
 class SpaceMissions::Mission
-  attr_accessor :name, :full_name, :url, :description, :type, :status, :launch_date, :launch_location, :end_date, :target, :current_location, :events, :key_discoveries, :scientific_instruments, :all
+  attr_accessor :name, :full_name, :url, :description, :type, :status, :launch_date, :launch_location, :end_date, :target, :current_location, :events, :key_discoveries, :scientific_instruments, :mission_links
 
-@@all = [] #array of all missions
+  @@all = [] #array of all missions
 
-  def self.scrape_missions
-    doc = Nokogiri::html(open("https://www.jpl.nasa.gov/missions/"))
+  def self.get_JPL_mission_links
+    @mission_links = []
+    doc = Nokogiri::HTML(open("https://www.jpl.nasa.gov/missions/"))
+    slides = doc.css('ul.articles li.slide')
+    slides.each do |slide|
+      mission_links<< slide.css('a').attribute('href').to_s
+    end
+    @mission_links
+  end
+
+  def attributes
+
+
     #go to jpl https://www.jpl.nasa.gov/missions/
     #create mission object from name, full name and url
     #scrape url for all other attributes
-    mission1 = self.new
-    #scrape JPL
-    mission_1.name = "AcrimSat"
-    mission_1.full_name = "Active Cavity Irradiance Monitor Satellite"
-    mission_1.url = "https://www.jpl.nasa.gov/missions/active-cavity-irradiance-monitor-satellite-acrimsat/"
-    #from url
+    # mission1 = self.new
+    # #scrape JPL
+    # mission_1.name = "AcrimSat"
+    # mission_1.full_name = "Active Cavity Irradiance Monitor Satellite"
+    # mission_1.url = "https://www.jpl.nasa.gov/missions/active-cavity-irradiance-monitor-satellite-acrimsat/"
+    # #from url
     # mission_1.description = "The Active Cavity Irradiance Monitor Satellite, or AcrimSat, mission spent 14 years in orbit monitoring Earth's main energy source, radiation from the sun, and its impacts on our planet. The satellite's ACRIM 3 instrument was the third in a series of satellite experiments that have contributed to a critical data set for understanding Earth's climate: a 36-year, continuous satellite record of variations in total solar radiation reaching Earth, or total solar irradiance. Solar irradience creates winds, heats the land and drives ocean currents, and therefore contains significant data that climatologists can use to improve predictions of climate change and global warming.
     #
     # Launched on Dec. 21, 1999, for a planned five-year mission, AcrimSat went silent on Dec. 14, 2013. Attempts since then to reestablish contact have been unsuccessful. The venerable satellite most likely suffered an expected, age-related battery failure."
@@ -32,16 +45,16 @@ class SpaceMissions::Mission
     # mission_1.key_discoveries = "Data from the mission helped researchers formulate global climate models and study solar physics."
     #
     # mission_1.scientific_instruments = ["Active Cavity Irradiance Monitor 3 (ACRIM 3)"]
-    @@all << self
+    # @@all << self
+    #
+    # mission_2 = self.new
+    # mission_2.name = "ASTER"
+    # mission_2.full_name = "Advanced Spaceborne Thermal Emission and Reflection Radiometer"
+    # mission_2.url = "https://www.jpl.nasa.gov/missions/advanced-spaceborne-thermal-emission-and-reflection-radiometer-aster/"
+    # @@all << self
+    #
+    # @@all
 
-    mission_2 = self.new
-    mission_2.name = "ASTER"
-    mission_2.full_name = "Advanced Spaceborne Thermal Emission and Reflection Radiometer"
-    mission_2.url = "https://www.jpl.nasa.gov/missions/advanced-spaceborne-thermal-emission-and-reflection-radiometer-aster/"
-    @@all << self
-
-    @@all
-  end
 
 
   def self.all
