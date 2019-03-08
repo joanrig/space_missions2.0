705de19f86927bdf2c38de52eb78bca62950a35f
joan <joanrigdon@gmail.com>
2019-03-07 21:51:01 -0500

    -m

--- lib/space_missions/mission.rb
+++ lib/space_missions/mission.rb
@@ -1,5 +1,5 @@
 class SpaceMissions::Mission
-  attr_accessor :url, :acronym, :name, :description, :attributes, :type, :status, :launch_date, :launch_location, :mission_end_date, :end_date, :target, :current_location, :altitude, :scientific_instruments
+  attr_accessor :url, :acronym, :name, :description, :destination, :attributes, :type, :status, :landing_date, :launch_date, :launch_location, :mission_end_date, :end_date, :target, :current_location, :altitude, :scientific_instruments
 
   @@all = [] #array of all missions
 
--- lib/space_missions/scraper.rb
+++ lib/space_missions/scraper.rb
@@ -5,8 +5,8 @@ class SpaceMissions::Scraper
     slides = @@doc.css('ul.articles li.slide')
     slides.each do |slide|
       mission = SpaceMissions::Mission.new
-      mission.url = slide.css('a').attribute('href').to_s.gsub("http", "https")
-      mission.name = @@doc.css('h1.media_feature_title').text.strip
+      mission.url = slide.css('a').attribute('href').value.gsub("http", "https")
+      mission.name = slide.css('h4').text
     end
   end
 
@@ -16,33 +16,16 @@ class SpaceMissions::Scraper
 
       #from text
       mission.description = doc.css('div.wysiwyg_content p').text.delete("\r").gsub("\n\n", "\n")
-      attributes = doc.css('ul.fast_facts li').text.delete("\t").delete("\n").gsub("\r", "  ").split("  ")
-      #still need mission events, key discoveries and array of scientific instruments
-      #binding.pry
+      attributes = doc.css('ul.fast_facts li')
 
-      #from fast_facts box
-      #[1] pry(SpaceMissions::Scraper)> attributes
-        # attributes=>
-        #["Acronym: AcrimSat",
-        #  "Type: Orbiter",
-        #  "Status: Past",
-        #  "Launch Date: December 20, 1999",
-        #  "Launch Location: Vandenberg Air Force Base, California",
-        #  "Mission End Date: August 08, 2014",
-        #  "Target: Earth",
-        #  "Current Location: Earth Orbit",
-        #  "Altitude: 713 km (apogee), 672 km (perigee)"]
       attributes.each do |el|
-        key = el.split(":")[0].downcase.gsub(" ","_")
-        value = el.split(":")[1].strip
-        # with .strip error =>/Users/Joan/Dev/space_missions/lib/space_missions/scraper.rb:22:in `block (2 levels) in get_attributes': undefined method `strip' for nil:NilClass (NoMethodError)
-        #without .strip, error=> `_april_03,_2013=' (a value!) gets sent as key
-        #if i make it: value = el.split(":")[1].strip if el.split(":")[1] ...then most values end up as nil
+        key = el.children.children.text.split(":")[0].downcase.gsub(" ", "_")
+        value = el.children.children.text.split(":")[1].delete("\r").delete("\n").strip
         mission.send("#{key}=", value)
+        binding.pry
       end
       mission
     end
-    binding.pry
   end
 
   def self.mission_links
