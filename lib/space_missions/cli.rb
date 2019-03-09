fc39567aeb41ea2d337db98b5362901f14fbe753
joan <joanrigdon@gmail.com>
2019-03-08 20:31:58 -0500

    breaking up CLI into smaller methods; listing all mission data under reconstruction

--- lib/space_missions/cli.rb
+++ lib/space_missions/cli.rb
@@ -1,6 +1,7 @@
 class SpaceMissions::CLI
 
   def call
+    get_data
     list_missions
     menu
     goodbye
@@ -8,16 +9,15 @@ class SpaceMissions::CLI
 
   @@target = nil
 
-  def list_missions
+  def get_data
     SpaceMissions::Scraper.get_jpl_mission_links
-    puts "just finished #get_jpl_mission_links"
-
-    sleep(1)
-    puts "... Hang on, we're loading data from #{SpaceMissions::Mission.all.size} missions for you!"
-
+    puts "... Hang on, we're getting data from #{SpaceMissions::Mission.all.size} missions for you!"
     SpaceMissions::Scraper.get_attributes
-    puts "just finished #get_attributes"
+    puts "just finished getting data"
+  end
 
+  def list_missions
+    puts "listing missions"
     i = 0
     SpaceMissions::Mission.all.each_with_index do |mission, index|
       if mission.acronym == nil
@@ -25,40 +25,57 @@ class SpaceMissions::CLI
       else
         puts "#{index+1}. #{mission.acronym} - #{mission.name}"
         i+= 1
-      #binding.pry
+        #binding.pry
       end
     end
-
-    puts "That's a lot of space exploration!"
-    sleep (3)
   end
 
   def menu
+    puts "hello from menu"
     input = nil
     while input != "exit"
       input = gets.strip.downcase
-      puts "Enter the number of the mission you'd like to learn more about. You can also type 'list' to see the missions again, or type 'exit' "
-
+      # puts "Enter the number of the mission you'd like to learn more about."
+      # puts "You can also type 'commands' for a complete list of commands, or 'exit' to return to earth."
       if input.to_i > 0
-        mission = SpaceMissions::Mission.all[0..5][input.to_i - 1]
-        # attributes = ["type", "status", "launch_date", "launch_location", "mission_end_date", "target", "current_location", "altitude"]
-        # attributes.each do |attr|
-        #   puts "#{attr.gsub("_", " ").capitalize}: #{mission.attr)}"
-        # end
-
+        mission = SpaceMissions::Mission.all[input.to_i - 1]
+        mission.info
+        binding.pry
       elsif input ==  "list"
-        SpaceMissions::Mission.all[0..5].each_with_index.tap
+        list_missions
+      elsif input == "commands"
+        commands
+      elsif input == "target"
+        SpaceMissions::Mission.target
+      elsif input == "type"
+        SpaceMissions::Mission.type
+      elsif input == "launch"
+        SpaceMissions::Mission.launch
+      elsif input == "end"
+        SpaceMissions::Mission.end
       else
-        puts "Whoops! That's not a number of a mission. Please try again."
+        puts "Whoops! That's not a valid command. Please try again."
+        commands
       end
     end
   end
 
-  def select
-    input = nil
+  def commands
+    puts "Here are some other commands you can try:"
+    puts ""
+    puts "'list' => see the missions again"
+    puts "'target' => search the missions by target (planet, universe, etc.)"
+    puts "'type' => search missions by type (orbiter, lander, rover, etc)"
+    puts "'launch date' => search missions by launch year"
+    puts "'end' => search missions by end of mission year"
+    puts ""
+    puts "What would you like to do?"
+  end
+
+  def select(input=nil)
     while input != "exit"
       input = gets.strip.downcase
-    targets = []
+
     end
   end
 
--- lib/space_missions/mission.rb
+++ lib/space_missions/mission.rb
@@ -1,6 +1,5 @@
 class SpaceMissions::Mission
-  attr_accessor  :acronym, :altitude, :attributes, :current_location, :description, :destination, :end_date,  :landing_date, :launch_date, :launch_location, :mission_end_date, :name, :status, :target, :type, :url,
-  :data
+  attr_accessor  :acronym, :altitude, :attributes, :current_location, :description, :destination, :end_date,  :info, :landing_date, :launch_date, :launch_location, :mission_end_date, :name, :status, :target, :type, :url
 
   @@all = [] #array of all missions
 
@@ -8,8 +7,25 @@ class SpaceMissions::Mission
     @@all << self
   end
 
-  def self.target(target)
-    self.select{|x| x[:target] == "#{SpaceMissions::CLI.target}".map{|y| y[:data]}}
+  def self.info
+    puts "#{mission.name}:"
+    puts "Acronym: #{mission.acronym}" if mission.acronym
+    puts "Description: #{mission.description}" if mission.description
+    puts "Type: #{mission.type}" if mission.type
+    puts "Launch Date: #{mission.launch_date}" if mission.launch_date
+    puts "Launch Location: #{mission.launch_location}" if mission.launch_location
+    puts "Landing Date: #{mission.landing_date}" if mission.landing_date
+    puts "End Date: #{mission.end_date}" if mission.end_date
+    puts "Mission End Date: #{mission.mission_end_date}" if mission.mission_end_date
+    puts "Target of interest: #{mission.target}" if mission.target
+    puts "Destination: #{mission.destination}" if mission.destination
+    puts "Current Loction: #{mission.current_location}" if mission.current_location
+    puts "Altitude: #{mission.altitude}" if mission.altitude
+  end
+
+  def self.find()
+    self.all.select{|mission| mission[:target] == "#{target}".map{|y| y[:name]}}
+    #binding.pry
   end
 
 
